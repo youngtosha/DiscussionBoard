28798234a686f2704b637e7f5cb60764f7fb9daa
youngtosha <anton.golosienko.programming@gmail.com>
2025-03-05 12:44:45 +0300

    Изменен секретный ключ для подключения к БД config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+SECRET_KEY = os.getenv("SECRET_KEY", "random_secret_key")
